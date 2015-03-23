c4da593d5d14cd29f6f9328292461a0be60a8403
Simon Sun <blackghost0118@gmail.com>
2015-03-23 23:00:31 +0800

    使用套件每 30 分鐘定時更新資料

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
+
+// Node.js modules
 var express = require('express');
 var app = express();
+var schedule = require('node-schedule');
 
+
+// Defined output data
+var outputData;
+
+
+// Library
 var reservoir = require('./libs/reservoir');
 
-app.get('/', function (req, res) {
 
-    reservoir(function (err, reservoirData) {
-        if (err) { 
-            return res.jsonp({ err: err.toString() });
+// Cron job for update output data
+var updateData = schedule.scheduleJob('* */30 * * *', function(){
+    reservoir(function (err, reservoirData){
+
+        if (err) {
+            console.log('---------------- ERROR ----------------');
+            console.log(err.toString());
+            console.log('---------------- ERROR ----------------');
         }
 
-        return res.jsonp({ data: reservoirData });
+        outputData = reservoirData;
+        console.log('---------------- SUCCESS ----------------');
+        console.log('UPDATE OUTPUT DATA SUCCESS');
+        console.log('---------------- SUCCESS ----------------');
+        return;
+    });
+});
+
+
+// Api router
+app.get('/', function(req, res) {
+
+    if(outputData){
+        return res.jsonp({
+            data: outputData
+        });
+    }
+
+    reservoir(function (err, reservoirData) {
+
+        if (err) {
+            return res.jsonp({
+                err: err.toString()
+            });
+        }
+
+        outputData = reservoirData;
+
+        return res.jsonp({
+            data: outputData
+        });
     });
 
 });
--- test/reservoir.js
+++ test/reservoir.js
@@ -3,7 +3,7 @@ var reservoir = require('../libs/reservoir');
 
 describe('reservoir', function () {
     it('should return reservoirData', function (done) {
-      	reservoir(done);
+        reservoir(done);
     });
 });
 
