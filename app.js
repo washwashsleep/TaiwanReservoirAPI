0d73c18b73e27e342851d127caaa1c35a141dd95
xhriman <seal789ie@gmail.com>
2015-04-23 21:13:52 +0800

    Immediate Data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var schedule = require('node-schedule');
 var debug = require('debug')('reservoir');
+var jsonQuery = require('json-query');
 
 
 // Defined output data
@@ -13,6 +14,7 @@ var outputData;
 // Library
 var reservoir = require('./libs/reservoir');
 var reservoir_today = require('./libs/reservoir_today');
+var reservoir_immediate = require('./libs/reservoir_immediate');
 
 
 // Cron job for update output data
@@ -72,6 +74,49 @@ app.get('/today', function(req, res) {
 
         return res.jsonp({
             data: reservoirData
+        });
+    });
+
+});
+
+app.get('/immediate',function(req, res){
+    //Merge the daily reservoir data with much more immediate data
+    reservoir(function (err, reservoirData) {
+        if (err)
+        {
+            return res.jsonp({
+                err: err.toString()
+            });
+        }
+    
+        reservoir_immediate(function (err, immediateData) {
+            if (err)
+            {
+                return res.jsonp({
+                    err: err.toString()
+                });
+            }
+
+            immediateData.forEach(function (element, index, array) {
+                var key = jsonQuery('reserv[reservoirName=' + element.reservoirName + ']', {
+                    data: {
+                        reserv: reservoirData
+                    }
+                }).key;
+                
+                if (key !== null) {
+                    if (reservoirData[key]) {
+                        reservoirData[key].lastedUpdateTime = element.lastedUpdateTime;
+                        reservoirData[key].immediateLevel = element.immediateLevel;
+                        reservoirData[key].immediateStorage = element.immediateStorage;
+                        reservoirData[key].immediatePercentage = element.immediatePercentage;
+                    }
+                }
+            });
+
+            return res.jsonp({
+                data: reservoirData
+            });
         });
     });
 
